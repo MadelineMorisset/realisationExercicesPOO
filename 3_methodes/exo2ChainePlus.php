8a0b4f91d05ab085fc64413d11a4a102a7fc329f
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-07-04 13:13:56 +0200

    Ajout commentaires exercice 2

--- 3_methodes/exo2ChainePlus.php
+++ 3_methodes/exo2ChainePlus.php
@@ -18,21 +18,41 @@
     // Mettre le résultat dans une page htmlisée
 
     class chainePlus {
+        // Initalisation de l'attribut "$_chaine" avec pour valeur, la chaine de caractère "Programmation orientée objet en PHP" :
         private $_chaine="Programmation orientée objet en PHP";
 
+        // La méthode "gras" prend l'argument "$debut" (qui correspond à la chaine de caractères envoyée comme valeur : $chaineComplete->gras("Gras : ")) :
         public function gras($debut) {
+            // $debut = "Gras : ";
+            // ouverture balise HTML strong
+            // appel de l'attribut $_chaine
+            // fermeture balise HTML strong et retour à la ligne
             return $debut . "<strong>" . $this->_chaine . "</strong><br>";
         }
 
+        // La méthode "italique" prend l'argument "$debut" (qui correspond à la chaine de caractères envoyée comme valeur : $chaineComplete->italique("Italique : ")) :
         public function italique($debut) {
+            // $debut = "Italique : ";
+            // ouverture balise HTML i
+            // appel de l'attribut $_chaine
+            // fermeture balise HTML i et retour à la ligne
             return $debut . "<i>" . $this->_chaine . "</i><br>";
         }
 
+        // La méthode "souligne" prend l'argument "$debut" (qui correspond à la chaine de caractères envoyée comme valeur : $chaineComplete->souligne("Souligné : ")) :
         public function souligne($debut) {
+            // $debut = "Souligné : ";
+            // ouverture balise HTML u
+            // appel de l'attribut $_chaine
+            // fermeture balise HTML u et retour à la ligne
             return $debut . "<u>" . $this->_chaine . "</u><br>";
         }
 
+        // La méthode "majuscules" prend l'argument "$debut" (qui correspond à la chaine de caractères envoyée comme valeur : $chaineComplete->majuscules("Majuscules : ")) :
         public function majuscules($debut) {
+            // $debut = "Majuscules : ";
+            // usage de la fonction  strtoupper()
+            // appel de l'attribut $_chaine
             return $debut.strtoupper($this->_chaine);
         }
     }
